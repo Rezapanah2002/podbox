88629f5224248b221b1e13209bc609f55f6e4030
rezapanah2002 <rezapanah2002@gmail.com>
2024-08-13 12:08:34 +0330

    faq

--- src/views/faq/Faq.jsx
+++ src/views/faq/Faq.jsx
@@ -3,9 +3,11 @@ import React from "react";
 function Faq() {
   return (
     <div>
-      <div className="grid justify-items-center m-10">
-        <h1>برای راهنمایی و پاسخ به سوالات شما آماده ایم</h1>
-        <h3>شاید برای شما هم سوال شده باشد!</h3>
+      <div className="grid justify-items-center p-28 gap-7">
+        <h1 className="text-5xl">
+          برای راهنمایی و پاسخ به سوالات شما آماده ایم
+        </h1>
+        <h3 className="text-xl">شاید برای شما هم سوال شده باشد!</h3>
       </div>
       <div className="flex justify-center gap-16 m-10">
         <div className="grid justify-items-center opacity-40 hover:opacity-70">
