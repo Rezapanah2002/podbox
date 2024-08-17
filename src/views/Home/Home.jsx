d388778e074bd6fe366fc1987c29cfb9ccadede3
rezapanah2002 <rezapanah2002@gmail.com>
2024-08-17 17:24:20 +0330

    animation start

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,22 +1,31 @@
+"use client";
+
 import React from "react";
+import { motion } from "framer-motion";
 
 function Home() {
   return (
     <div>
-      <section className="flex justify-center p-28 bg-custom-gradient h-screen w-full">
+      <section className="flex flex-col items-center justify-evenly p-32 bg-custom-gradient gap-32 h-full w-full">
         <div className="grid justify-items-center whitespace-pre-wrap">
-          <h1 className="text-6xl text-center font-thin w-5/6 max-[745px]:text-2xl max-[745px]:w-full">
+          <h1 className="text-6xl text-center font-thin w-5/6 max-[745px]:text-3xl max-[745px]:w-full">
             پادباکس، دوستی برای سرگرمی و زندگی هوشمند
           </h1>
-          <h3 className="text-2xl text-wrap text-center w-1/2 max-[745px]:text-base max-[745px]:w-full">
+          <h3 className="text-2xl text-wrap text-center w-1/2 max-[745px]:text-lg max-[745px]:w-full">
             شما که دنبال استریم سرگرمی و آوردن هوشمندی دیجیتال به خانه و
             زندگی‌تان هستید، شما پادباکس را کم دارید
           </h3>
           <a href="https://app.podbox.ir/" target="_blank">
-            <button className="p-4 border-2 bg-w-b border-b-from rounded-xl hover:scale-110 duration-500">
+            <button className="m-10 p-5 border-2 bg-w-b border-b-from rounded-xl hover:scale-110 duration-500">
               ورود به وب اپلیکیشن پادباکس
             </button>
           </a>
+        </div>
+        {/* animation */}
+        <div className="w-full h-full flex justify-center">
+          <motion.div className="w-4/5 h-96 bg-gradient-to-r from-b-from via-b-via to-b-to rounded-2xl">
+            {/* <img src="../../../anim-bg.png" /> */}
+          </motion.div>
         </div>
       </section>
     </div>
